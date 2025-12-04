49a9179462fe65c4e2c25c7e92ee9231f06b08dc
GantangSatria <gantang610@gmail.com>
2025-12-04 09:14:30 +0700

    fix(ui): fixed ui error

--- src/components/buat-janji/BuatJanjiClient.jsx
+++ src/components/buat-janji/BuatJanjiClient.jsx
@@ -155,22 +155,9 @@ export default function BuatJanji() {
                     type="date"
                     value={selectedDate}
                     onChange={(e) => setSelectedDate(e.target.value)}
-                    className="w-full px-4 py-3 pr-12 border border-gray-300 rounded-xl bg-white text-gray-700"
+                    className="w-full px-4 py-3 border border-gray-300 rounded-xl bg-white text-gray-700"
                   />
 
-                  <span className="absolute right-2 md:right-4 top-1/2 -translate-y-1/2 text-gray-400">
-                    <svg
-                      width="18"
-                      height="18"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <rect x="3" y="4" width="18" height="18" rx="2" />
-                      <path d="M16 2v4M8 2v4M3 10h18" />
-                    </svg>
-                  </span>
                 </div>
               </div>
 
